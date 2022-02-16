e11646982bc1402fd6a2ee58f25d92057afc3fc6
AlmasT10 <92239068+AlmasT10@users.noreply.github.com>
2022-02-16 17:09:48 -0500

    almas

--- app/src/main/java/com/example/vegun_buddy/MainActivity.kt
+++ app/src/main/java/com/example/vegun_buddy/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.FrameLayout
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
        val toolbar : MaterialToolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
